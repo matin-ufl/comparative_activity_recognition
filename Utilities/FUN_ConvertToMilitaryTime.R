e5e37940605730559ad3789e04087dc1f48ea3a3
avirup88 <avirup88.univ@gmail.com>
2017-06-08 04:08:06 -0400

    Add files via upload

---
+++ Utilities/FUN_ConvertToMilitaryTime.R
@@ -0,0 +1,25 @@
+
+# This function converts a time string to its military (24hr) format.
+# Examples:
+#          a) 2:14:30  -->  14:14:30
+#          b) 8:25:00  -->  08:25:00
+ConvertToMilitaryTime <- function(timeStr) {
+  if(is.na(timeStr)){
+    return(NA)
+  }
+  timeStr <- trimws(timeStr, which = "both")
+  timeStr <- unlist(strsplit(timeStr, " "))[1]
+  tokens <- unlist(strsplit(timeStr, ":"))
+  if(length(tokens) < 3) {
+    return(NA)
+  }
+  hour <- as.numeric(as.character(tokens[1]))
+  if(hour <= 7) {
+    hour <- hour + 12
+  }
+  hour <- as.character(hour)
+  if(nchar(hour) < 2) {
+    hour <- paste("0", hour, sep = "")
+  }
+  return(paste(as.character(hour), tokens[2], tokens[3], sep = ":"))
+}
---
+++ Utilities/SCP_ConvertTaskTimeFile.R
@@ -0,0 +1,40 @@
+#This script converts the taskTimes_all.csv file into taskTimes_all.Rdata file 
+#by including only the necessary columns required for the analysis
+
+
+#Set the working directory to the loaction where this script is located 
+
+setwd("~/Desktop/Data_Mining_Project/Data_Cleaning_Code/My_Code")
+
+#Load the function file
+
+source("FUN_ConvertToMilitaryTime.R")
+
+#Set the directory to the location where the taskTimes_all.csv file is located
+
+dataFolder <- "~/Desktop/Data_Mining_Project/Raw_Data/"
+
+taskTimesFileName <- paste(dataFolder,"taskTimes_all.csv", sep = "")
+  
+#Check if the file exists in the given directory. 
+
+if(!file.exists(taskTimesFileName)) {
+  stop("No TaskTimes csv file found.") 
+} else {
+  print ("Tasktimes csv file found. Converting csv into Rdata file....")
+}
+
+#Load the taskTimes file into a R dataFrame 
+taskTimes.df <- read.csv(file = taskTimesFileName, header = T)
+colnames(taskTimes.df) <- c("task", "cosmed.start", "cosmed.end", "phone.start", "phone.end", "PID", "visit", "visit.date")
+taskTimes.df$start.time <- sapply(taskTimes.df$phone.start, FUN = ConvertToMilitaryTime)
+taskTimes.df$end.time <- sapply(taskTimes.df$phone.end, FUN = ConvertToMilitaryTime)
+taskTimes.df <- taskTimes.df[, c(1,6:10)]
+taskTimes.df <- taskTimes.df[complete.cases(taskTimes.df), ]
+
+#Save the R dataframe into a Rdata file 
+saveRDS(taskTimes.df, file = paste(dataFolder,"taskTimes_all.Rdata",sep = ""))
+
+rm(taskTimes.df)
+
+message("Tasktimes csv file read and converted into a Rdata file.")
