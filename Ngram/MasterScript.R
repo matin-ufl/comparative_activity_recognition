491ade4589741fae82d8e1494bc063713f4ca281
Shikha Mehta <shikha.mehta@ufl.edu>
2017-06-22 14:46:36 -0400

    Removing redundant code that uses taskTimes file data

--- Ngram/MasterScript.R
+++ Ngram/MasterScript.R
@@ -1,17 +1,10 @@
 # Master Script: Construct unigram and bigram features for all the participants and save the aggregate Rdata files
 
-# Set the full path where taskTimes and participants' Rdata files are located
-mainFolder <- "C:/Users/shikh/Documents/University of Florida/Activity Recognition/Datasets/Raw Data/"
+# Set the full path where participants' Rdata files are located.
+dataFolder <- "C:/Users/shikh/Documents/University of Florida/Activity Recognition/Datasets/Raw Data/Testing_Set/"
 
 # Set the working directory to the location where the scripts and function R files are located
 setwd("C:/Users/shikh/Documents/University of Florida/Activity Recognition/DataCleanUp")
-
-# Fetch task times for all participants into a data frame
-taskTimes.df <- readRDS(file = paste(mainFolder,"taskTimes_all.Rdata", sep = ""))
-print("Tasktimes file found and read.")
-
-# Select data folder (i.e. Training/Testing) for which ngram features are to be generated
-dataFolder<-paste(mainFolder,"Training_Set/",sep="")
 
 source("ConstructNgramsForParticipant.R")
 participantsList <- dir(dataFolder, pattern = "^.*.Rdata$")
@@ -27,8 +20,7 @@ for(participantFile in participantsList) {
   print (paste("Data successfully loaded for Participant: ",participantID))
   
   # Call function to construct features from current participant's data
-  participantFeatures.df <- nGrams.oneParticipant(participantID = participantID,
-                                           ppt.df = participantWristData,taskTimes.df = taskTimes.df[taskTimes.df$PID == participantID,])
+  participantFeatures.df <- nGrams.oneParticipant(participantID = participantID,ppt.df = participantWristData)
   
   # Merge participant features to the main data frame
   UnigramFeatures.df <- rbind.fill(UnigramFeatures.df, participantFeatures.df[[1]])
