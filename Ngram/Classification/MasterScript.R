9bfaff5cb7235f55da4332fbde7ae902c53796e4
Shikha Mehta <shikha.mehta@ufl.edu>
2017-07-06 15:09:32 -0400

    Added SVM and Naive Bayes

--- Ngram/Classification/MasterScript.R
+++ Ngram/Classification/MasterScript.R
@@ -1,3 +1,5 @@
+# Classification results
+
 setwd("C:/Users/shikh/Documents/University of Florida/Activity Recognition/Classification/")
 source("Classifiers.R")
 
@@ -10,11 +12,20 @@ testing.df<-prepare_for_classification(dataFolder,"Bigrams")
 names <- colnames(training.df)
 Missing <- setdiff(names, names(testing.df))  # Find names of missing columns in testing.df
 testing.df[Missing] <- 0                    # Add them, filled with 0s
+
+# ---Comment this section for SVM as the model may run into zero variability issues--- #
 names <- colnames(testing.df)
 Missing <- setdiff(names, names(training.df))  # Find names of missing columns in training.df
 training.df[Missing] <- 0                    # Add them, filled with 0s
-
+# ------------------------------------------------------------------------------------ #
+  
 rm(names,Missing)
+
+# SVM =================================================
+output_SVM.df<-svm_classify(training.df,testing.df)
+
+# Naive Bayes ========================================
+output_NB.df<-naive_bayes_classify(training.df,testing.df)
 
 # Random Forest =========================================
 output_RF.df<-random_forest_classify(training.df,testing.df)
