a0dc13c46a2355f12c14f758a8f7db95091e4708
Shikha Mehta <shikha.mehta@ufl.edu>
2017-07-08 17:34:14 -0400

    Update and rename FUN_ConstructNgramsForParticipant.R to FUN_Construct_Ngrams.R
    Added comments and header

--- Ngram/Data Cleaning/FUN_Construct_Ngrams.R
+++ Ngram/Data Cleaning/FUN_Construct_Ngrams.R
@@ -1,14 +1,15 @@
-# Converts accelerometer data for a participant into Representative Patterns (Unigrams and Bigrams).
-# 1. The total number of bins is defined as 10. We compute "binInterval" such that data for all visits can be distributed into the 10 bins.
-# 2. For every task, we define a logical array. Let's call it "Unigrams".
-#     2.1. each accelerometer data point (VM) in our dataset is checked.
-#     2.2. for each data point, we assign a bin number (equal to accelerometer value / binInterval) in 'Unigrams'.
-#     2.3. we define a 2-points sliding window, such as <p_1, p_2>.
-#     2.4. the pattern for <Unigrams[p_1], Unigram[p_2]> is read. E.g., <1, 0>
-#     2.5. this 2-points pattern is converted into one "Bigram".E.g., <10>
-#     2.6. the 2-point window is shifted by 1 seconds. (we keep a 1-second overlapping point)
-#     2.7. the process is repeated (go to 2.4) unless we reach the last second of the task.
-#
+#Author & Reviewer Details --------------------------------------------------------------------------------------
+
+#Author : Shikha Mehta
+#Date : 07/08/2017
+#E-Mail : shikha.mehta@ufl.edu
+#Reviewed By : Madhurima Nath
+#Review Date : 
+#Reviewer E-Mail : madhurima09@ufl.edu
+
+#----------------------------------------------------------------------------------------------------------------
+
+
 # Input:
 #      1. PID: participant unique identifier
 #      2. Task: task title
@@ -21,10 +22,23 @@
 #      1. PID: participant unique identifier
 #      2. Task: task title
 #      3. Unigram/Bigram patterns: features for the task
-
+#
+# Converts accelerometer data for a participant into Representative Patterns (Unigrams and Bigrams).
+# ALGORITHM:
+# 1. The total number of bins is received as input. We compute "binInterval" 
+#    such that data for all visits can be distributed into all the bins.
+# 2. For every task, we define a logical array. Let's call it "Unigrams".
+#     2.1. each accelerometer data point (VM) in our dataset is checked.
+#     2.2. for each data point, we assign a bin number (equal to accelerometer value / binInterval) in 'Unigrams'.
+#     2.3. we define a 2-points sliding window, such as <p_1, p_2>.
+#     2.4. the pattern for <Unigrams[p_1], Unigram[p_2]> is read. E.g., <1, 0>
+#     2.5. this 2-points pattern is converted into one "Bigram".E.g., <10>
+#     2.6. the 2-point window is shifted by 1 seconds. (we keep a 1-second overlapping point)
+#     2.7. the process is repeated (go to 2.4) unless we reach the last second of the task.
+#
 nGrams.construction <- function(PID, Task, taskData, binInterval,binCount) {
   
-  result <- list(data.frame(matrix(nrow = 0, ncol = 0),stringsAsFactors=FALSE),data.frame(matrix(nrow = 0, ncol = 0),stringsAsFactors=FALSE))
+  result.dfs <- list(data.frame(matrix(nrow = 0, ncol = 0),stringsAsFactors=FALSE),data.frame(matrix(nrow = 0, ncol = 0),stringsAsFactors=FALSE))
   
   Unigrams <- character(length = length(taskData))
   j <- 1
@@ -44,19 +58,19 @@ nGrams.construction <- function(PID, Task, taskData, binInterval,binCount) {
     i <- i + 1
   }
   
-  U <- as.data.frame(table(Unigrams),stringsAsFactors=FALSE)
-  UG <- as.data.frame(t(U),stringsAsFactors=FALSE)
-  UnigramFeatureSet <- as.data.frame(UG[-1,],stringsAsFactors=FALSE)
-  colnames(UnigramFeatureSet) <- unlist(UG[1,],use.names = TRUE)
+  U.df <- as.data.frame(table(Unigrams),stringsAsFactors=FALSE)
+  UG.df <- as.data.frame(t(U.df),stringsAsFactors=FALSE)
+  UnigramFeatureSet.df <- as.data.frame(UG.df[-1,],stringsAsFactors=FALSE)
+  colnames(UnigramFeatureSet.df) <- unlist(UG.df[1,],use.names = TRUE)
   
-  B <- as.data.frame(table(Bigrams),stringsAsFactors=FALSE)
-  BG <- as.data.frame(t(B),stringsAsFactors=FALSE)
-  BigramFeatureSet <- as.data.frame(BG[-1,],stringsAsFactors=FALSE)
-  colnames(BigramFeatureSet) <- unlist(BG[1,],use.names = TRUE)
+  B.df <- as.data.frame(table(Bigrams),stringsAsFactors=FALSE)
+  BG.df <- as.data.frame(t(B.df),stringsAsFactors=FALSE)
+  BigramFeatureSet.df <- as.data.frame(BG.df[-1,],stringsAsFactors=FALSE)
+  colnames(BigramFeatureSet.df) <- unlist(BG.df[1,],use.names = TRUE)
   
   participantInfo=data.frame(PID = PID, Task = Task,stringsAsFactors=FALSE)
-  result <- list(merge(participantInfo,UnigramFeatureSet), merge(participantInfo,BigramFeatureSet))
-  result
+  result.dfs <- list(merge(participantInfo,UnigramFeatureSet.df), merge(participantInfo,BigramFeatureSet.df))
+  result.dfs
 }
 
 
@@ -69,6 +83,7 @@ nGrams.construction <- function(PID, Task, taskData, binInterval,binCount) {
 #     Returns a list of two data frames: 
 #     1. Unigram features for each <Participant, Task>.
 #     2. Bigram features for each <Participant, Task>.
+#
 nGrams.oneParticipant <- function(participantID, ppt.df, binCount) {
   print(paste("Constructing n-grams for", participantID))
   require(plyr)
@@ -79,7 +94,7 @@ nGrams.oneParticipant <- function(participantID, ppt.df, binCount) {
   # Calculating bin interval for the current participant
   binInterval<-(max(ppt.df$VM)-minVM)/binCount
   
-  result <- list(data.frame(matrix(nrow = 0, ncol = 0),stringsAsFactors=FALSE),data.frame(matrix(nrow = 0, ncol = 0),stringsAsFactors=FALSE))
+  result.dfs <- list(data.frame(matrix(nrow = 0, ncol = 0),stringsAsFactors=FALSE),data.frame(matrix(nrow = 0, ncol = 0),stringsAsFactors=FALSE))
   
   # Constructing features #
   for(task in unique(ppt.df$TaskLabel)){
@@ -88,28 +103,36 @@ nGrams.oneParticipant <- function(participantID, ppt.df, binCount) {
     
     nGrams.df <- nGrams.construction(PID = participantID, Task = task, taskDataFrame$VM-minVM, binInterval,binCount) 
     if(nrow(nGrams.df[[1]]) > 0) {
-        result[[1]] <- rbind.fill(result[[1]], nGrams.df[[1]])
+        result.dfs[[1]] <- rbind.fill(result.dfs[[1]], nGrams.df[[1]])
     }
     if(nrow(nGrams.df[[2]]) > 0) {
-       result[[2]] <- rbind.fill(result[[2]], nGrams.df[[2]])
+       result.dfs[[2]] <- rbind.fill(result.dfs[[2]], nGrams.df[[2]])
     }
   }
-  result[[1]][is.na(result[[1]])] <- 0
-  result[[2]][is.na(result[[2]])] <- 0
+  result.dfs[[1]][is.na(result.dfs[[1]])] <- 0
+  result.dfs[[2]][is.na(result.dfs[[2]])] <- 0
   print(paste("Constructed n-gram features for", participantID))
-  result
+  result.dfs
 }
 
+
+# Input:
+#     features.df: combined feature set of all participants to process
+#
+# Output:
+#     Returns a data frame with non-NA values and "B" appended to all feature columns
+#
 adjustFeatureSet<-function(features.df)
 {
   # Update all NAs to 0
   features.df[is.na(features.df)] <- 0
   
-  #Append feature names with "B" and convert feature data to type - numeric
   FeatureNames<-names(features.df)
   for(i in 3:ncol(features.df))
   {
+    # Append feature name with "B"
     FeatureNames[[i]]<-as.character(paste("B",FeatureNames[[i]],sep=""))
+    # Convert feature data to type - numeric
     features.df[,i]<-as.numeric(features.df[,i])
   }
   colnames(features.df) <- FeatureNames
