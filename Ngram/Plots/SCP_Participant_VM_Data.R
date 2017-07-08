2f359dce73f84e2d1dab71ffae9ec7a5bfe63d11
Shikha Mehta <shikha.mehta@ufl.edu>
2017-07-08 17:39:32 -0400

    Update and rename SCP_ParticipantVMData.R to SCP_Participant_VM_Data.R

---
+++ Ngram/Plots/SCP_Participant_VM_Data.R
@@ -0,0 +1,7 @@
+PARTICIPANTDATAPATH <- readRDS(file="C:/Users/shikh/Documents/University of Florida/Activity Recognition/Datasets/Raw Data/Training_Set/ADAD151_downsampled_Data.Rdata")
+attach(PARTICIPANTDATAPATH)
+
+par(mfrow=c(1,3))
+plot(VM[TaskLabel=="DIGGING"],ylim=c(min(VM),max(VM)),las=1,type="l",ylab="Vector Magnitude",xlab="",main="Participant ADAD151 - Digging",col=2,col.main=2,cex.main=1.5,cex.lab=1.5)
+plot(VM[TaskLabel=="LEISURE WALK"],ylim=c(min(VM),max(VM)),las=1,type="l",ylab="Vector Magnitude",xlab="",main="Participant ADAD151 - Leisure Walk",col=3,col.main=3,cex.main=1.5,cex.lab=1.5)
+plot(VM[TaskLabel=="TV WATCHING"],ylim=c(min(VM),max(VM)),las=1,type="l",ylab="Vector Magnitude",xlab="",main="Participant ADAD151 - TV Watching",col=4,col.main=4,cex.main=1.5,cex.lab=1.5)
